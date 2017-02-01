9a06b78d68e35205f125a5edb958b3d1d6857bca
Alex Ward <alex@volcaniccreations.com>
2017-02-01 17:20:17 +0000

    finished refining the filter behavour. I still need to add some more styling to it though

--- src/scripts/components/filter.js
+++ src/scripts/components/filter.js
@@ -1,40 +1,56 @@
 /* global $ */
 
-let filter = $('.js-filter');
 
+// Set up the variables
+let filter = $('.js-filter');
+let filterList = [];
 let form = filter.find('form').addClass('collapse');
 let articleContainer = $('.js-article-list');
 
+// This is a generic function that can be moved to a utility if it is needed another component. All it does is swap one class for another on both the clicked target and an optional additional target.
 let toggleClass = function(event) {
   let needle = event.data.needle;
-  let element = needle.length ? needle : event.currentTarget;
+  let element = needle.length ? needle : $(event.currentTarget);
   let origClass = event.data.origClass;
   let altClass = event.data.altClass;
 
   if (element.hasClass(origClass)) {
+    $(event.currentTarget).removeClass(origClass).addClass(altClass);
     element.removeClass(origClass).addClass(altClass);
   } else {
+    $(event.currentTarget).removeClass(altClass).addClass(origClass);
     element.removeClass(altClass).addClass(origClass);
   }
 }
 
-let filterList = [];
-
+// Takes the event from a form update and shows or hides the appropriate sections on the page.
+// Note: This doesn't save the state between sessions or page refreshes.
 let doFilter = function(event) {
   let el = $(event.currentTarget);
   let needle = el.attr('name');
   articleContainer.find('article').hide();
+
+  // check to see which items are checked and add those items to the filterList array.
   if(el.is(':checked')) {
+    el.parents('li').addClass('checked');
     filterList.push(needle);
   } else {
+    el.parents('li').removeClass('checked');
     filterList = filterList.filter(item => item !== needle);
   }
-  console.log(filterList);
-  for (let article in filterList) {
-    if(article) {
-      articleContainer.find('article[data-slug="'+filterList[article]+'"]').show();
+
+  // If the array has items in then filter by the array, if not then show all the articles.
+  if (filterList.length > 0) {
+    for (let article in filterList) {
+      if(article) {
+        articleContainer.find('article[data-slug="'+filterList[article]+'"]').show();
+      }
     }
+  } else {
+    articleContainer.find('article').show();
   }
+
+
 }
 
 filter.find('.title').on('click', { origClass: 'collapse', altClass: 'expand', needle: form },  toggleClass);
