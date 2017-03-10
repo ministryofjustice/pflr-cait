f4397f9c9f2d5956298afaa1a92bdc8b598cadf2
Alex Robinson <github@alex.fu2k.org>
2017-03-10 15:34:07 +0000

    Tweak jenkins unit test script so it will actually run

--- ci/jenkins/unit-tests-run.sh
+++ ci/jenkins/unit-tests-run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-if [[ "$SKIP_LINT" != "true" || "$SKIP_UNIT" != "true" ]]
+NOSKIP=false
+if [ "$SKIP_LINT" != "true" ]; then NOSKIP=true; fi
+if [ "$SKIP_UNIT" != "true" ]; then NOSKIP=true; fi
+
+if [ $NOSKIP = true ]
 then
-  DOCKERTAG=$JOB_NAME-$BUILD_NUMBER
+  DOCKERTAG=$(echo $JOB_NAME-$BUILD_NUMBER | tr '[:upper:]' '[:lower:]')
   docker build -t=$DOCKERTAG .
   if [ "$SKIP_LINT" != "true" ]
   then
