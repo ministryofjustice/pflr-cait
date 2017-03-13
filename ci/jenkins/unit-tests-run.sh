d52eee9709d3ff874c25b4282c0175e01eaaf3d8
Alex Robinson <github@alex.fu2k.org>
2017-03-13 12:52:43 +0000

    Add usage to jenkins job scripts

--- ci/jenkins/unit-tests-run.sh
+++ ci/jenkins/unit-tests-run.sh
@@ -1,5 +1,45 @@
 #!/bin/sh
 
+if [[ $1 == "-h" || $1 == "--help" ]]
+then
+  USAGE=$(cat << EndOfMessage
+unit-tests-run.sh
+=================
+
+Runs unit tests in a docker container
+
+Used by Jenkins job https://ci.service.dsd.io/view/family%20justice/job/BUILD-cait
+
+The following environment variables must be set:
+
+  JOB_NAME
+
+  BUILD_NUMBER
+
+The following environment variables can also be set:
+
+  SKIP_LINT
+  whether to run linting or not
+
+  SKIP_UNIT
+  whether to run unit tests or not
+EndOfMessage
+  )
+  echo "$USAGE"
+  exit 0
+fi
+
+if [ "$JOB_NAME" == "" ]
+then
+  echo Environment variable JOB_NAME must be set
+  exit 1
+fi
+if [ "$BUILD_NUMBER" == "" ]
+then
+  echo Environment variable BUILD_NUMBER must be set
+  exit 1
+fi
+
 NOSKIP=false
 if [ "$SKIP_LINT" != "true" ]; then NOSKIP=true; fi
 if [ "$SKIP_UNIT" != "true" ]; then NOSKIP=true; fi
